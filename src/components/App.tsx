304d656281397f9c249665f438d7547bd79258e7
azyobuzin <azyobuzin@users.sourceforge.jp>
2022-07-17 01:27:05 +0900

    AboutButton

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,18 @@
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import {
+  AppBar,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
-import { FC } from "react";
+import { FC, useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectAddress, selectPolarCoordinates } from "../store";
 import classes from "./App.module.css";
@@ -15,6 +27,7 @@ const App: FC = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             東京極座標
           </Typography>
+          <AboutButton />
         </Toolbar>
       </AppBar>
       <Box height="100%">
@@ -45,5 +58,43 @@ const Footer: FC = () => {
         <TweetButton polarCoordinates={polarCoordinates} address={address} />
       </Box>
     </Box>
+  );
+};
+
+const AboutButton: FC = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = useCallback(() => setOpen(true), []);
+  const handleClose = useCallback(() => setOpen(false), []);
+
+  return (
+    <>
+      <IconButton size="large" color="inherit" onClick={handleOpen}>
+        <InfoOutlinedIcon />
+      </IconButton>
+      <Dialog
+        aria-labelledby="about-dialog-title"
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle id="about-dialog-title">東京極座標について</DialogTitle>
+        <DialogContent>
+          <Typography gutterBottom>
+            「東京の地名覚えられないから皇居を原点とした極座標で示すべき」
+          </Typography>
+          <Typography gutterBottom>
+            東京極座標は、東京周辺の場所を、皇居を原点とした極座標に変換するサービスです。
+            結局東京のどこらへんだよ！の説明に困ったときに、ご利用ください。
+          </Typography>
+          <Typography variant="body2">
+            <Link href="https://twitter.com/azyobuzin" target="_blank">
+              作者: @azyobuzin
+            </Link>
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>閉じる</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
